178125762e7a731c4105ab3d88292f906d90f6c2
root <orzalter@outlook.com>
2019-12-27 11:45:56 +0800

    delete Serializer.dump decode

--- app/models/users.py
+++ app/models/users.py
@@ -27,7 +27,7 @@ class User(db.Model):
 
     def generate_auth_token(self, expiration):
         s = Serializer(current_app.config['SECRET_KEY'], expires_in=expiration)
-        return s.dumps({'id': self.id}).decode('utf-8')
+        return s.dumps({'id': self.id})
 
     @staticmethod
     def verify_auth_token(token):
